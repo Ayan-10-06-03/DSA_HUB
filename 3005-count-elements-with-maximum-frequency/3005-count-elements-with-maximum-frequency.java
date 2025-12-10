22803d4b6436700d03ca67224fde29dca89af331
Aditya Ayan Singh <adityaayan.singh2026@gmail.com>
2025-12-10 22:27:36 +0530

    Time: 2 ms (69.26%), Space: 44.1 MB (13.49%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,31 +1,30 @@
 class Solution 
 {
-    public int maxFrequencyElements(int[] nums) 
-    {
+    public int maxFrequencyElements(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : nums)
-        {
-            map.put(i, map.getOrDefault(i, 0)+1);
+
+        // count frequencies
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int num : map.values())
-        {
-            list.add(num);
+        int maxFreq = 0;
+
+        // find max frequency
+        for (int freq : map.values()) {
+            maxFreq = Math.max(maxFreq, freq);
         }
 
-        Collections.sort(list);
-        int fc = list.get(list.size()-1);
-        int max = list.get(list.size()-1);
+        int total = 0;
 
-        for (int i=list.size()-2; i>=0; i--)
-        {
-            if (list.get(i) == max)
-            {
-                fc = fc + list.get(i);
+        // count total occurrences of elements having max frequency
+        for (int freq : map.values()) {
+            if (freq == maxFreq) {
+                total += freq;
             }
         }
 
-        return fc;
+        return total;
     }
+
 }
