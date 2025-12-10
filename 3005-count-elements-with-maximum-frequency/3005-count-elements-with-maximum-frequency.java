2679bddd252f0730c2a820de870da5c9639a3677
Aditya Ayan Singh <adityaayan.singh2026@gmail.com>
2025-12-10 22:35:47 +0530

    Time: 5 ms (4.98%), Space: 44.4 MB (6.31%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,30 +1,37 @@
 class Solution 
 {
     public int maxFrequencyElements(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(nums);
 
-        // count frequencies
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        int maxFreq = 1;
+        int currFreq = 1;
+
+        // Step 1: Find maximum frequency
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                currFreq++;
+            } else {
+                maxFreq = Math.max(maxFreq, currFreq);
+                currFreq = 1;
+            }
         }
+        maxFreq = Math.max(maxFreq, currFreq); // last element group
 
-        int maxFreq = 0;
-
-        // find max frequency
-        for (int freq : map.values()) {
-            maxFreq = Math.max(maxFreq, freq);
-        }
-
+        // Step 2: Count total occurrences of all elements with maximum frequency
+        currFreq = 1;
         int total = 0;
 
-        // count total occurrences of elements having max frequency
-        for (int freq : map.values()) {
-            if (freq == maxFreq) {
-                total += freq;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                currFreq++;
+            } else {
+                if (currFreq == maxFreq) total += currFreq;
+                currFreq = 1;
             }
         }
 
+        if (currFreq == maxFreq) total += currFreq; // last group
+
         return total;
     }
-
 }
